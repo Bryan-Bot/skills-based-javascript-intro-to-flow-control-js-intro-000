062d6b1e3fbc504121af7cc90fd890fd85f94a4d
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-05-25 20:27:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,17 +12,32 @@ else {
   return ("You are not a teenager")
 }
 }
+
+
 function ageChecker(age) {
   if (13>=age && age<=19) {
     return("You are a teenager!")
   }
-  else if (age<=12) {
+  else if (12>=age) {
     return("You are a kid")
   }
   else if (age>19) {
     return("You are a grownup")
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 function ternaryTeenager(age) {
   if (age>=13 && age<=19) {
   return("You are a teenager")
